b6facaa9645dc40cd31cdd56065f5d323325760c
ialan034 <ialan034@gmail.com>
2020-05-01 15:27:35 -0400

    pom update

--- src/test/java/com/zerobank/runners/CucumberRunner.java
+++ src/test/java/com/zerobank/runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/zerobank/stepDefinitions",
-        tags = "@login",
+        tags = "",
         dryRun = false,
         strict = true,
         plugin = {
