ded55b3eba44aa2f13e7c99c972e448a72260c65
ialan034 <ialan034@gmail.com>
2020-05-05 09:22:35 -0400

    documents feature updated

--- src/test/java/com/zerobank/runners/CucumberRunner.java
+++ src/test/java/com/zerobank/runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/zerobank/stepDefinitions",
-        tags = "",
+        tags = "@statements_documents",
         dryRun = false,
         strict = true,
         plugin = {
